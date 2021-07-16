5bdcb363de8cd2921d52d0106769c9975fd06518
Anand Vijay <anand.vijay@anchanto.com>
2021-07-16 17:04:30 +0530

    Create Article module completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,19 @@ class ArticlesController < ApplicationController
   def index
     @articles=Article.all
   end
+
+  def new
+    @article=Article.new
+  end
+  
+  def create
+    @article=Article.new(params.require( :article).permit(:Title , :Description , :author))
+    if @article.save
+      redirect_to articles_path()
+      flash[:notice]="Articles saved sucessfully."
+    else
+      render 'new'
+    end
+  end
+
 end
